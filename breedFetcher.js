da716421d5cdebf82596d0a48cfe30e568119cdb
Edward <edward.ascan@me.com>
2021-01-14 22:52:17 +0000

    wrote code for breedFetcher and testing

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (data[0]) {
+      callback(null, data[0].description);
+   
+    } else {
+      callback(null, 'breed is not found');
+    }
+
+  });
+};
+
+
+
+module.exports = { fetchBreedDescription };
